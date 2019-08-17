b88eb908afeb775064cdde597bfeefdcebac795d
narcisskylake <narcisskylake@gmail.com>
2019-08-17 13:24:42 +0800

    [Update] Fixed choices shuffle feature

--- js/app.js
+++ js/app.js
@@ -17,91 +17,131 @@ angular.module('tApp', []);
         tc.questions = {
             items: [
                 {
-                    question: '1st Question',
+                    question: 'People generally invest their money to provide:',
                     extraDetails: [
-                        'i',
-                        'ii',
-                        'iii',
-                        'iv',
-                        'v',
+                        'I. An improvement in their financial position',
+                        'II. A less comfortable standard of living',
+                        'III. Retirement income',
+                        'IV. Funds for paying necessary expenses and taxes when the person dies'
                     ],
                     options: [
                         {
                             'key' : 'a',
-                            'value' : '1st choice'
+                            'value' : 'I, II and III'
                         },{
                             'key' : 'b',
-                            'value' : '2nd choice'
+                            'value' : 'I, III and IV'
                         },{
                             'key' : 'c',
-                            'value' : '3rd choice'
+                            'value' : 'I, II and IV'
                         },{
                             'key' : 'd',
-                            'value' : '4th choice'
+                            'value' : 'II, III and IV'
                         }
                     ],
-                    answer: 3
+                    answer: 'b'
                 },
                 {
-                    question: '2nd Question',
-                    extraDetails: [
-                        'i',
-                        'ii',
-                        'iii',
-                        'iv',
-                        'v',
-                    ],
+                    question: 'Which of the following funds is comprised of a higher proportion of equity and a lower proportion of fixed-income',
+                    extraDetails: [],
                     options: [
                         {
                             'key' : 'a',
-                            'value' : '1st choice'
+                            'value' : 'Bond Funds'
                         },{
                             'key' : 'b',
-                            'value' : '2nd choice'
+                            'value' : 'Cash Funds'
                         },{
                             'key' : 'c',
-                            'value' : '3rd choice'
+                            'value' : 'Managed Funds'
                         },{
                             'key' : 'd',
-                            'value' : '4th choice'
+                            'value' : 'Mixed Funds'
                         }
                     ],
-                    answer: 1
+                    answer: 'c'
                 },
                 {
-                    question: '3rd Question',
+                    question: 'Which of the following are the main characteristics of Variable Life insurance policies?',
                     extraDetails: [
-                        'i',
-                        'ii',
-                        'iii',
-                        'iv',
-                        'v',
+                        'I. The policies can be used for investments, as a source of regular savings and protection.',
+                        'II. The withdrawal and protection benefit are determined by the investment performance of the underlying',
+                        'III. The net withdrawal values of the policies are the gross withdrawal values shown in the policy which'
                     ],
                     options: [
                         {
                             'key' : 'a',
-                            'value' : '1st choice'
+                            'value' : 'I only'
                         },{
                             'key' : 'b',
-                            'value' : '2nd choice'
+                            'value' : 'II only'
                         },{
                             'key' : 'c',
-                            'value' : '3rd choice'
+                            'value' : 'I and II only'
                         },{
                             'key' : 'd',
-                            'value' : '4th choice'
+                            'value' : 'I, II and III'
                         }
                     ],
-                    answer: 2
+                    answer: 'c'
+                },
+                {
+                    question: 'Which of the following statements are FALSE?',
+                    extraDetails: [
+                        'I. The policyowners may request a partial withdrawal of the policy and the amount will be met by cashing the units at the offer price.',
+                        'II. The structure of charges and the investment content of a Variable Life policy are specified in the policy document and the policy statement.',
+                        'III. Some Variable Life policies grant loans to policyowners which is limited to a percentage of the cash value.',
+                        'IV. Commissions and office expenses are met by a variety of implicit charges, some of which are variable.'
+                    ],
+                    options: [
+                        {
+                            'key' : 'a',
+                            'value' : 'I and II only'
+                        },{
+                            'key' : 'b',
+                            'value' : 'I and III only'
+                        },{
+                            'key' : 'c',
+                            'value' : 'II and III only'
+                        },{
+                            'key' : 'd',
+                            'value' : 'All of the above'
+                        }
+                    ],
+                    answer: 'b'
+                },
+                {
+                    question: 'Which of the following statements about the feature of Regular Premium Variable Life Policy are TRUE?',
+                    extraDetails: [
+                        'I. Top-ups are usually allowed.',
+                        'II. The level of coverage can be varied.',
+                        'III. Premium holidays are usually allowed.'
+                    ],
+                    options: [
+                        {
+                            'key' : 'a',
+                            'value' : 'I and II only'
+                        },{
+                            'key' : 'b',
+                            'value' : 'I and III only'
+                        },{
+                            'key' : 'c',
+                            'value' : 'II and III only'
+                        },{
+                            'key' : 'd',
+                            'value' : 'I, II and III'
+                        }
+                    ],
+                    answer: 'd'
                 }
             ]
         };
-        
-        
+
+
         tc.userAnswers = [];
 
-        
-        
+
+
         tc.currentQuestionCount = 0;
         tc.currentQuestionItem = {
             question: '',
@@ -109,10 +149,10 @@ angular.module('tApp', []);
             options: {},
             answer: ''
         };
-        
+
         tc.currentAnswer = {
             questionOrder: -1,
-            answerOrder: -1
+            answer: ''
         };
 
         tc.totalScore = 0;
@@ -121,7 +161,7 @@ angular.module('tApp', []);
 
         tc.isQuizStarted = false;
         tc.startQuiz = startQuiz;
-        
+
         tc.nextQuestion = nextQuestion;
         tc.initCurrentQuestionItem = initCurrentQuestionItem;
         tc.saveCurrentAnswer = saveCurrentAnswer;
@@ -130,10 +170,10 @@ angular.module('tApp', []);
         tc.initTotalQuestionItems = initTotalQuestionItems;
         tc.isLastQuestion = isLastQuestion;
         tc.isComplete = isComplete;
-        
+
         tc.checkUserAnswers = checkUserAnswers;
         tc.getQuestion = getQuestion;
-        tc.getUserAnswer = getUserAnswer;
+        tc.getUserAnswerText = getUserAnswerText;
         tc.getCorrectAnswer = getCorrectAnswer;
 
         tc.shuffleQuestionsAnswers = shuffleQuestionsAnswers;
@@ -157,16 +197,16 @@ angular.module('tApp', []);
             tc.currentQuestionItem = tc.questions.items[tc.currentQuestionCount];
             tc.clearCurrentAnswer();
         }
-        
+
         function saveCurrentAnswer() {
             tc.currentAnswer.questionOrder = tc.currentQuestionCount;
             tc.userAnswers.push(tc.currentAnswer);
         }
-        
+
         function clearCurrentAnswer() {
             tc.currentAnswer = {
                 questionOrder: -1,
-                answerOrder: -1
+                answer: ''
             };
         }
 
@@ -181,35 +221,55 @@ angular.module('tApp', []);
         function isComplete() {
             return tc.currentQuestionCount === tc.totalQuestionItems;
         }
-        
-        
+
+
         function checkUserAnswers() {
+            console.log(tc.userAnswers);
 
             tc.userAnswers.forEach(function (item, index) {
-                if(item.answerOrder == (tc.questions.items[item.questionOrder].answer - 1)) {
+                if(item.answer === (tc.questions.items[item.questionOrder].answer)) {
                     tc.totalScore++;
                 }
             });
+
+            console.log(tc.totalScore);
+
         }
 
         function getQuestion(item) {
             return tc.questions.items[item.questionOrder].question;
         }
 
-        function getUserAnswer(item) {
-            return tc.questions.items[item.questionOrder].options[item.answerOrder].value;
+        function getUserAnswerText(item) {
+            let userAnswerText = '';
+            let thisQUestionOptions = tc.questions.items[item.questionOrder].options;
+
+            thisQUestionOptions.forEach(function (options, index) {
+                if(options.key === item.answer) {
+                    userAnswerText = options.value;
+                }
+            });
+
+            return userAnswerText;
         }
-        
+
         function getCorrectAnswer(item) {
-            let userAnswer = item.answerOrder;
-            let correctAnswer = tc.questions.items[item.questionOrder].answer - 1;
-            let correctAnswerText = tc.questions.items[item.questionOrder].options[tc.questions.items[item.questionOrder].answer - 1].value;
+            let userAnswer = item.answer;
+            let correctAnswer = tc.questions.items[item.questionOrder].answer;
+
+            let correctAnswerText = '';
+
+            tc.questions.items[item.questionOrder].options.forEach(function (options, index) {
+                if(options.key === correctAnswer) {
+                    correctAnswerText = options.value;
+                }
+            });
 
             return $sce.trustAsHtml(userAnswer == correctAnswer ?
                 '<span class="text-success">Your answer is correct!</span>'
                 : '<span class="text-success">Correct answer: </span>' + correctAnswerText);
         }
-        
+
 
         function shuffleQuestionsAnswers() {
             let questionItemsCopy = tc.questions.items;
